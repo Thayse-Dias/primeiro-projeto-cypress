5f9709c51a360de0354775013fbee347c9d1d1ce
Thayse <thayse.dias@gmail.com>
2025-09-25 09:49:55 -0300

    Added selectors list for login spec

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -1,27 +1,36 @@
 // ====== Cenários de Testes de Login ======
 
-//Utilizando o selector por atributo, através do comando 'get' para localizar os elementos na página.
+const { selectFields } = require("express-validator/lib/field-selection")
 
-//Cenário de Teste 1: Login com Sucesso 
+// Utilizando o selector por atributo, através do comando 'get' para localizar os elementos na página.
 
 describe('Orange HRM Tests', () => {
+
+  const selectorsList = {
+    usernameField: "[name='username']",
+    passwordField: "[name='password']",
+    loginButton: "[type='submit']",
+    selectionTitleTopBar: ".oxd-topbar-header-breadcrumb-module",
+    wrongCredentialsAlert: ".oxd-alert",
+  }
+
+  // Cenário de Teste 1: Login com Sucesso 
   it('Login - Sucess', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get('input[name="username"]').type('Admin')
-    cy.get('input[name="password"]').type('admin123')
-    cy.get('button[type="submit"]').click()
+    cy.get(selectorsList.usernameField).type('Admin')
+    cy.get(selectorsList.passwordField).type('admin123')
+    cy.get(selectorsList.loginButton).click()
     cy.location('pathname').should('equal', '/web/index.php/dashboard/index') 
-    cy.get('.oxd-topbar-header-breadcrumb-module').contains('Dashboard')
-
+    cy.get(selectorsList.selectionTitleTopBar).contains('Dashboard')
   })
-})
 
-//Cenário de Teste 2: Login sem Sucesso 
-
+  // Cenário de Teste 2: Login sem Sucesso 
   it.only('Login - Fail', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get('input[name="username"]').type('Test')
-    cy.get('input[name="password"]').type('Test123')
-    cy.get('button[type="submit"]').click()
-    cy.get('.oxd-alert').contains('Invalid credentials')
+    cy.get(selectorsList.usernameField).type('Test')
+    cy.get(selectorsList.passwordField).type('Test123')
+    cy.get(selectorsList.loginButton).click()
+    cy.get(selectorsList.wrongCredentialsAlert).contains('Invalid credentials')
   })
+
+})
