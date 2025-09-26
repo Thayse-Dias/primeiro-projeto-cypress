c26610a5ff5e7856405d1bd5b0cb4c18fe9466d8
Thayse <thayse.dias@gmail.com>
2025-09-26 10:02:12 -0300

    substuição do arquivo login para user.spec.cy.js

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -10,20 +10,23 @@ describe('Orange HRM Tests', () => {
     passwordField: "[name='password']",
     loginButton: "[type='submit']",
     selectionTitleTopBar: ".oxd-topbar-header-breadcrumb-module",
-    dashboardGrid: ".orangehrm-dashboard-grid",
+    dashboardGrid: ".orangehrm-dashboard-grid", //Substituindo a verificação do título do Dashboard
     wrongCredentialsAlert: ".oxd-alert",
+    myInfoButton: "[href='/web/index.php/pim/viewMyDetails']"
   }
 
   // Cenário de Teste 1: Login com Sucesso 
   //baseUrl (cy.visit) definida no cypress.config.js
   // Utilizando o selector por atributo, através do comando 'get' para localizar os elementos na página.
-  it.only('Login - Sucess', () => {
+  
+  it.only('User Info Update - Sucess', () => {
     cy.visit('/auth/login')
     cy.get(selectorsList.usernameField).type(userData.userSucess.username) 
     cy.get(selectorsList.passwordField).type(userData.userSucess.password)
     cy.get(selectorsList.loginButton).click()
     cy.location('pathname').should('equal', '/web/index.php/dashboard/index') 
     cy.get(selectorsList.dashboardGrid)
+    cy.get(selectorsList.myInfoButton).click() //Acessando a página 'My Info' do site
   })
 
   // Cenário de Teste 2: Login sem Sucesso 
