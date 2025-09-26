b3ac319a80915cd35e7eb578ccd079806e0753c3
Thayse <thayse.dias@gmail.com>
2025-09-26 09:15:18 -0300

    base url added

--- cypress.config.js
+++ cypress.config.js
@@ -9,6 +9,11 @@ module.exports = defineConfig({
     json: true,
   },
   e2e: {
-    // ... outras configurações
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    baseUrl: 'https://opensource-demo.orangehrmlive.com/web/index.php',
+    specPattern: 'cypress/e2e/**/*.cy.js',
+    supportFile: 'cypress/support/e2e.js',
   },
 });
--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -1,10 +1,7 @@
 // ====== Cenários de Testes de Login ======
 
 import userData from '../fixtures/userData.json'
-
-const { selectFields } = require("express-validator/lib/field-selection")
-
-// Utilizando o selector por atributo, através do comando 'get' para localizar os elementos na página.
+//Const userData importado do arquivo userData.json
 
 describe('Orange HRM Tests', () => {
 
@@ -18,9 +15,11 @@ describe('Orange HRM Tests', () => {
   }
 
   // Cenário de Teste 1: Login com Sucesso 
+  //baseUrl (cy.visit) definida no cypress.config.js
+  // Utilizando o selector por atributo, através do comando 'get' para localizar os elementos na página.
   it.only('Login - Sucess', () => {
-    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get(selectorsList.usernameField).type(userData.userSucess.username)
+    cy.visit('/auth/login')
+    cy.get(selectorsList.usernameField).type(userData.userSucess.username) 
     cy.get(selectorsList.passwordField).type(userData.userSucess.password)
     cy.get(selectorsList.loginButton).click()
     cy.location('pathname').should('equal', '/web/index.php/dashboard/index') 
@@ -29,9 +28,9 @@ describe('Orange HRM Tests', () => {
 
   // Cenário de Teste 2: Login sem Sucesso 
   it('Login - Fail', () => {
-    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get(selectorsList.usernameField).type('Test')
-    cy.get(selectorsList.passwordField).type('Test123')
+    cy.visit('/auth/login')
+    cy.get(selectorsList.usernameField).type(userData.userFail.username)
+    cy.get(selectorsList.passwordField).type(userData.userFail.password)
     cy.get(selectorsList.loginButton).click()
     cy.get(selectorsList.wrongCredentialsAlert).contains('Invalid credentials')
   })
