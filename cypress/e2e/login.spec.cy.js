95f01a4220418f683463b8e3d2a33e452be536d2
Thayse <thayse.dias@gmail.com>
2025-09-26 08:49:21 -0300

    Adding user data json

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -1,5 +1,7 @@
 // ====== Cenários de Testes de Login ======
 
+import userData from '../fixtures/userData.json'
+
 const { selectFields } = require("express-validator/lib/field-selection")
 
 // Utilizando o selector por atributo, através do comando 'get' para localizar os elementos na página.
@@ -11,21 +13,22 @@ describe('Orange HRM Tests', () => {
     passwordField: "[name='password']",
     loginButton: "[type='submit']",
     selectionTitleTopBar: ".oxd-topbar-header-breadcrumb-module",
+    dashboardGrid: ".orangehrm-dashboard-grid",
     wrongCredentialsAlert: ".oxd-alert",
   }
 
   // Cenário de Teste 1: Login com Sucesso 
-  it('Login - Sucess', () => {
+  it.only('Login - Sucess', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get(selectorsList.usernameField).type('Admin')
-    cy.get(selectorsList.passwordField).type('admin123')
+    cy.get(selectorsList.usernameField).type(userData.userSucess.username)
+    cy.get(selectorsList.passwordField).type(userData.userSucess.password)
     cy.get(selectorsList.loginButton).click()
     cy.location('pathname').should('equal', '/web/index.php/dashboard/index') 
-    cy.get(selectorsList.selectionTitleTopBar).contains('Dashboard')
+    cy.get(selectorsList.dashboardGrid)
   })
 
   // Cenário de Teste 2: Login sem Sucesso 
-  it.only('Login - Fail', () => {
+  it('Login - Fail', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
     cy.get(selectorsList.usernameField).type('Test')
     cy.get(selectorsList.passwordField).type('Test123')
