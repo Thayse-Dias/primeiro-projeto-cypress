c1cc2d6b386b77d31e2865662e9443c3043b41f4
Thayse <thayse.dias@gmail.com>
2025-09-28 11:16:48 -0300

    User update scenario added

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -10,13 +10,19 @@ describe('Orange HRM Tests', () => {
     passwordField: "[name='password']",
     loginButton: "[type='submit']",
     selectionTitleTopBar: ".oxd-topbar-header-breadcrumb-module",
-    dashboardGrid: ".orangehrm-dashboard-grid", //Substituindo a verificação do título do Dashboard
+    dashboardGrid: ".orangehrm-dashboard-grid", //Verificação do título do Dashboard
     wrongCredentialsAlert: ".oxd-alert",
-    myInfoButton: "[href='/web/index.php/pim/viewMyDetails']"
+    myInfoButton: "[href='/web/index.php/pim/viewMyDetails']",
+    firstNameField: "[name='firstName']",
+    lastNameField: "[name='lastName']",
+    genericField: ".oxd-input--active",
+    dateField: "[placeholder='yyyy-dd-mm']",
+    dateCloseButton: ".--close",
+    submitButton: "[type='submit']",
   }
 
-  // Cenário de Teste 1: Login com Sucesso 
-  //baseUrl (cy.visit) definida no cypress.config.js
+  // Cenário de Teste 1: Login com Sucesso e navegação até a página 'My Info'
+  // baseUrl (cy.visit) definida no cypress.config.js
   // Utilizando o selector por atributo, através do comando 'get' para localizar os elementos na página.
   
   it.only('User Info Update - Sucess', () => {
@@ -27,6 +33,15 @@ describe('Orange HRM Tests', () => {
     cy.location('pathname').should('equal', '/web/index.php/dashboard/index') 
     cy.get(selectorsList.dashboardGrid)
     cy.get(selectorsList.myInfoButton).click() //Acessando a página 'My Info' do site
+    cy.get(selectorsList.firstNameField).clear().type('FirstNameTest') //Atualizando o campo 'First Name'
+    cy.get(selectorsList.lastNameField).clear().type('LastNameTest') //Atualizando o campo 'Last Name'
+    cy.get(selectorsList.genericField).eq(3).clear().type('EmployeeId') //Atualizando o campo 'Employee Id'
+    cy.get(selectorsList.genericField).eq(4).clear().type('otherTest') 
+    cy.get(selectorsList.genericField).eq(5).clear().type('DriverLicenseTest')
+    cy.get(selectorsList.dateField).eq(0).clear().type('1983-03-26') //Atualizando o campo 'Date of Birth'
+    cy.get(selectorsList.dateCloseButton).click() //Fechando o calendário do campo 'Date of Birth'
+    cy.get(selectorsList.submitButton).eq(0).click() //Salvando as alterações
+    cy.get('body').should('contain.text', 'Successfully Updated') //Verificação da mensagem de sucesso
   })
 
   // Cenário de Teste 2: Login sem Sucesso 
